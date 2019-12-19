5dee7d129add2b4fea247a1407016ba906983ff2
Xianfeng <jiaoxianfeng_0509@buaa.edu.cn>
2019-12-20 04:36:55 +0800

    edit

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -25,7 +25,7 @@
                             <v-menu offset-y>
                                 <template v-slot:activator="{ on }">
                                     <v-img
-                                            src = "http://pics.sc.chinaz.com/files/pic/pic9/201907/bpic12885.jpg"
+                                            :src = "getPlayUrl"
                                             class="avatar"
                                             size="10"
                                             v-on="on"
