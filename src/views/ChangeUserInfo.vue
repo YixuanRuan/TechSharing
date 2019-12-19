f6b6cd7557e8701c2266ef77c1ae9439aa1312f3
Jongjyh <522963890@qq.com>
2019-12-20 04:18:53 +0800

    localstorage finished!

--- src/views/ChangeUserInfo.vue
+++ src/views/ChangeUserInfo.vue
@@ -1,7 +1,7 @@
 <template>
     <v-row>
         <v-col >
-            <v-card class="mx-auto"  outlined style="text-align: center;width: 1000px;height: 700px">
+            <v-card class="mx-auto"  outlined style="text-align: center;width: 1000px;height: 800px">
                 <v-row>
                     <div style="text-align: left" >
                             <div class="left">个人信息修改</div>
@@ -20,7 +20,14 @@
                                 style="opacity: 0.8;width: 300px "
                                 v-model="newid"
                         ></v-text-field>
+                        <v-alert type="error" v-if="emptyname">
+                            请输入新用户名
+                        </v-alert>
+                        <v-alert type="success" v-if="commitname">
+                            修改成功
+                        </v-alert>
                     </v-col>
+
                     <v-col cols="2"></v-col>
                     <v-col>
                         <v-btn
@@ -37,10 +44,7 @@
                         <div style="font-size: 20px">修改头像：</div>
                     </v-col>
                     <v-col cols="4">
-                        <v-file-input
-                                chips
-                                label="选择图片"
-                        ></v-file-input>
+                        <input class="file" name="file" type="file" accept="image/png,image/gif,image/jpeg" @change="update"/>
                     </v-col>
                     <v-col cols="2"></v-col>
                     <v-col>
@@ -88,6 +92,15 @@
                                     v-model="reppw"
                             ></v-text-field>
                         </v-row>
+                        <v-alert type="error" v-if="emptypw">
+                            请输入密码
+                        </v-alert>
+                        <v-alert type="error" v-if="notsame">
+                            密码输入不一致
+                        </v-alert>
+                        <v-alert type="success" v-if="commitpw">
+                            修改成功
+                        </v-alert>
                     </v-col>
                     <v-col cols="1"></v-col>
                     <v-col>
@@ -109,12 +122,19 @@
     export default {
         name: "Identification",
         mounted(){
+            this.$store.dispatch('changetoken',localStorage.getItem('token'))
         },
         components: {
             NavBar
         },
         data () {
             return{
+                file:{},
+                emptypw:false,
+                commitpw:false,
+                notsame:false,
+                emptyname:false,
+                commitname:false,
                 newid:'',
                 oldpw:'',
                 newpw:'',
@@ -128,12 +148,85 @@
             }
         },
         methods: {
-            applynewid: function(){
 
-            },applypw: function(){
+            update(e){
+                this.file = e.target.files[0];
+                console.log(this.file);
 
             },applypic: function(){
+                event.preventDefault();
+                let formData = new FormData()
+                formData.append('file', this.file)
+                let config = {
+                    headers: {
+                        token:localStorage.getItem('token'),
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+                this.$http.post(this.$store.state.baseurl+'/api/user/changeUserInfo', formData, config).then(function (response) {
+                        console.log(response);
+                })
+            },
 
+            applynewid: function(){
+                if(this.newid.length==0)
+                { this.emptyname=true
+                    this.commitname=false
+                }
+                else if(this.reppw==this.newpw)
+                {
+                    this.notsame=true;
+                    this.emptyname=false
+                }
+                else{
+                    this.notsame=false
+                    this.emptyname=false
+                    this.axios({
+                        method: 'post',
+                        url: this.$store.state.baseurl+'/api/user/changeUserInfo',
+                        headers: {
+                            token: this.$store.state.token
+                        },
+                        data: {
+                            "nickname":this.newid,
+                        },
+                        crossDomain: true
+                    }).then(body => {
+                        console.log(body)
+                    })
+                }
+
+            },applypw: function(){
+                if(this.newid.length==0)
+                { this.emptypw=true
+                    this.commitpw=false
+                    this.notsame=false
+                }
+                else if(this.reppw!=this.newpw)
+                {
+                    this.notsame=true;
+                    this.emptyname=false
+                    this.commitpw=false
+                }
+                else{
+                    this.commitpw=true
+                    this.notsame=false
+                    this.emptyname=false
+                    this.axios({
+                        method: 'post',
+                        url: this.$store.state.baseurl+'/api/user/changePsw',
+                        headers: {
+                            token: this.$store.state.token
+                        },
+                        data: {
+                            "oldpsw":this.oldpsw,
+                            "newpsw":this.newpw
+                        },
+                        crossDomain: true
+                    }).then(body => {
+                        console.log(body)
+                    })
+                }
             },selectpic: function(event){
 
             }
